8dfb87b4b93aeefc39e11990e6d022d3ba058b24
gusalm <gustaeazy@gmail.com>
2024-10-01 13:50:10 -0300

    SiteConfeitaria

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,6 +17,23 @@ def cardapio():
 @app.route("/contato")
 def contatos():
     return render_template("contatos.html")
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        username = request.form.get("")
+        password = request.form.get("password")
+
+
+    if username == "abcdefg":
+        if password == "1234567":
+            return redirect(url_for('upload'))
 
 if __name__ == "__main__":
     app.run(debug=True)
