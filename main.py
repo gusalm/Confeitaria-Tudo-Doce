719234caaada75190f2493e958d7bc5e9c0b89b4
gusalm <gustaeazy@gmail.com>
2024-10-02 10:41:05 -0300

    SiteConfeitaria

--- main.py
+++ main.py
@@ -27,13 +27,14 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
-        username = request.form.get("")
+        username = request.form.get("username")
         password = request.form.get("password")
 
+        if username == "abcdefg":
+            if password == "1234567":
+                return redirect(url_for('upload'))
 
-    if username == "abcdefg":
-        if password == "1234567":
-            return redirect(url_for('upload'))
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
